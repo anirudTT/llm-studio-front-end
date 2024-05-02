8fce1014be012590074fb2102cafd94c8eb11aaa
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-05-02 12:45:53 -0400

    more corrections

--- llm-studio-frontend/src/App.tsx
+++ llm-studio-frontend/src/App.tsx
@@ -1,17 +1,13 @@
 import "./App.css";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 
-import { Button } from "@/components/ui/button";
-
 import { NavBar } from "./components/Navbar";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <>
-        <div className="h-screen">
-          {/* <h1 className="text-3xl font-bold underline">Hello hot reload!</h1> */}
-          {/* <Button>Click me</Button> */}
+        <div className="h-screen flex items-center justify-center">
           <NavBar />
         </div>
       </>
--- llm-studio-frontend/src/components/Navbar.tsx
+++ llm-studio-frontend/src/components/Navbar.tsx
@@ -1,11 +1,12 @@
+import React from "react";
 import logo from "../assets/tt_logo.svg";
 import { DarkModeToggle } from "./DarkModeToggle";
 
 export function NavBar() {
   return (
-    <div className="flex items-center justify-between p-5 border-b-4 bg-gray-800 text-white">
-      <img src={logo} alt="LLM Studio Logo" className="w-16 h-16 mr-4" />
-      <h1 className="text-3xl font-semibold">LLM Studio</h1>
+    <div className="flex items-center py-5 px-10 border-b-4 bg-secondary">
+      <img src={logo} alt="Finetuner Logo" className="w-16 h-16 mr-4" />
+      <h1 className="text-4xl flex-grow text-white">Finetuner</h1>
       <DarkModeToggle />
     </div>
   );
