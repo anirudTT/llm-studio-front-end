c587261cb7bf352e1f3ee214ca9ca2508d241765
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-05-03 11:04:31 -0500

    initial working version

--- llm-studio-frontend/src/App.tsx
+++ llm-studio-frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +28,18 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <button className="bg-blue-900 hover:bg-blue-300 text-green font-bold py-2 px-4 rounded">
+        Test Button
+      </button>
+      <button className="bg-blue-500 hover:bg-red-500 text-white font-bold py-2 px-4 rounded transition-colors duration-300">
+        Click Me
+      </button>
+      <button className="bg-green-500 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded transition-colors duration-300">
+        Click Me
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
