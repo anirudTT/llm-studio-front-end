0f94ced8a6982f806bc73eb83dc314729187434b
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-05-02 22:51:42 -0500

    add initial stuff

--- new-build/src/App.tsx
+++ new-build/src/App.tsx
@@ -1,14 +1,13 @@
 import "./App.css";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 import { NavBar } from "./components/NavBar";
-import "./index.css";
+
 function App() {
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <div className="h-screen">
           <NavBar />
-          <h1 className="text-13xl font-bold underline">Hello world!</h1>
         </div>
       </ThemeProvider>
     </>
