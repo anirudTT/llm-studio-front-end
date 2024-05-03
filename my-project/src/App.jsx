3168a871db19baf100f56b821307619d8605ac30
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-05-03 10:50:33 -0500

    perfect vite+tailwind version

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,14 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Button } from "@/components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Button variant="outline">Button</Button>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -24,20 +22,17 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
       <button className="bg-blue-900 hover:bg-blue-300 text-green font-bold py-2 px-4 rounded">
         Test Button
       </button>
       <button className="bg-blue-500 hover:bg-red-500 text-white font-bold py-2 px-4 rounded transition-colors duration-300">
-        Click Me
-      </button>
-      <button className="bg-green-500 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded transition-colors duration-300">
         Click Me
       </button>
     </>
