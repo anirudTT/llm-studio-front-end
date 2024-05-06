fc9a3a514fc11de90c2483f39a41029759fc7c2a
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-05-06 09:06:11 -0400

    working with navbar and dark mode

--- llm-studio-frontend/src/components/NavBar.tsx
+++ llm-studio-frontend/src/components/NavBar.tsx
@@ -5,10 +5,22 @@ import { ModeToggle } from "./DarkModeToggle";
 export default function NavBar() {
   return (
     <div className="relative w-full">
-      <div className="flex justify-between items-center w-full px-5 py-3 bg-secondary border-b-4">
+      <div className="flex justify-between items-center w-full px-5 py-3 bg-secondary border-b-4 rounded-2xl shadow-xl">
         <div className="flex items-center">
-          <img src={logo} className="w-10 mr-4" alt="Logo" />
-          <h1 className="text-2xl">LLM STUDIO V0</h1>
+          <a
+            href="https://www.tenstorrent.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={logo}
+              className="w-10 h-10 mr-4 rounded-full shadow-inner transform transition duration-300 hover:scale-110"
+              alt="Logo"
+            />
+          </a>
+          <h1 className="text-2xl font-semibold text-gray-800 dark:text-white">
+            LLM STUDIO V0
+          </h1>
         </div>
         <ModeToggle />
       </div>
